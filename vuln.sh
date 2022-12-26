5cb7093c9e769aed090a51709f4acc890f2bb6bc
MR.HRCXXII <betazona1984@gmail.com>
2022-12-26 23:57:55 +0900

    Update vuln.sh

--- vuln.sh
+++ vuln.sh
@@ -2,20 +2,20 @@ clear
 figlet VULN |lolcat
 echo "\33[0;36m[==============================================\33[0;36m]"
 echo "\33[0;36m[        \33[31;1m✪Kumpulan Website VULN Deface✪        \33[0;36m]"
-echo "\33[0;36m[             \33[1;33m[$]Author : RoniYT               \33[0;36m]"
+echo "\33[0;36m[             \33[1;33m[$]Author : HRCXXII               \33[0;36m]"
 echo "\33[0;36m[         \33[1;33m[✓] Gunakan Dengan Bijak [✓]         \33[0;36m]"
 echo "\33[0;36m[==============================================\33[0;36m]"
 echo
 
-echo "\33[33;1m✪ZK1_Cyber INDONESIAN✪"
+echo "\33[33;1m✪ZK1_CyberXXII INDONESIAN✪"
 echo "\33[31;1m[1] \33[32;1mMelanjutkan"
 echo "\33[31;1m[2] \33[32;1mKeluar"
 echo
 echo "\33[31;1m[3] \33[32;1mInfo Tentang Saya"
 
-read -p "Masukan Nomor Pilihan :" roniyt
+read -p "Masukan Nomor Pilihan :" HRCXXII
 
-case $roniyt in
+case $HRCXXII in
 1)
 clear
 figlet ZK1 |lolcat
@@ -28,12 +28,12 @@ echo "\33[31;1mTerimakasih Telah Menggunakan Tool Saya"
 ;;
 3)
 clear
-echo "\33[32;1mHallo Nama Saya Roni"|lolcat
-echo "\33[32;1mSaya Adalah Anggota ZK1_Cyber INDONESIAN"|lolcat
+echo "\33[32;1mHallo Nama Saya MrXXII"|lolcat
+echo "\33[32;1mSaya Adalah Anggota CyberXXII INDONESIAN"|lolcat
 echo "\33[32;1mIngin Tutorial Lainnya ?"|lolcat
-echo "\33[32;1mCek Channel RoniYT"|lolcat
-echo "\33[32;1mLink : https://m.youtube.com/channel/UCNHZbNTS69kwCYJ8jcLzUzQ/"|lolcat
-echo "\33[1;33mSalam Hangat : Sahabat ZK1 :)"|lolcat
+echo "\33[32;1mCek Channel HRCXXII"|lolcat
+echo "\33[32;1mLink : https://www.youtube.com/@hrcreator22"|lolcat
+echo "\33[1;33mSalam Hangat : Sahabat CyberXXII :)"|lolcat
 ;;
 *)
 echo "Maaf Sahabat,Kamu Salah Masukan Nomor"
