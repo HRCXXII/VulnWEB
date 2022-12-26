b6641eb5f59165cf51d3b16934e484470f1ca6db
MR.HRCXXII <betazona1984@gmail.com>
2022-12-26 23:55:15 +0900

    Update README.php

--- README.php
+++ README.php
@@ -4,12 +4,12 @@
 ✓pkg install php
 ✓pkg install figlet
 ✓pip2 install lolcat
-✓git clone https://github.com/ZK1Hacker/vuln
-✓cd vuln
+✓git clone https://github.com/HRCXXII/VulnWEB
+✓cd vulnWEB
 ✓sh vuln.sh
 
-Jangan Lupa Subscribe Channel RoniYT
+Jangan Lupa Subscribe Channel HRCXXII
 Banyak Tutorial Tentang Hacking
-Link : https://m.youtube.com/channe✓pkg update
+Link : https://www.youtube.com/@hrcreator22
 
 
